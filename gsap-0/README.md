60519f97cf0ec8a7b9ce3da0b7c2ffee7d691654
Mohith <mohithog@gmail.com>
2024-09-01 22:24:09 +0530

    learnt basics of gsap

---
+++ gsap-0/README.md
@@ -0,0 +1,4 @@
+gsap .
+- to: animates intial state to final state - (from default values to defined values).       
+- from : vice-versa (starts with defined value  and animate towards default value ) .
+- timeline  : allows you to   sequence multiple animations.
---
+++ gsap-0/script.js
@@ -0,0 +1,19 @@
+var tl = gsap.timeline();
+tl.to("#box1" , {
+    x:1000,
+    rotate:360,
+    scale:0.5,
+    duration:2,
+})
+tl.to("#box2" , {
+    x:1000,
+    rotate:360,
+    scale:0.5,
+    duration:2,
+})
+tl.to("#box3" , {
+    x:1000,
+    rotate:360,
+    scale:0.5,
+    duration:2,
+})
